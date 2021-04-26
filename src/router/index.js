41bb9df51ce13e144583421fb441d1f395758811
Crashm <ismail.setiawan81@gmail.com>
2021-04-26 16:32:42 +0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,16 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../views/Dashboard.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <v-row>
+      <v-col v-for="(graph, index) in sales" :key="`${graph.title}-${index}`">
+        <SalesGraph :sales="sales" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col v-for="statistic in statistics" :key="`${statistic.title}`">
+        <StatisticCard :statistic="statistic" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import SalesGraph from "./component/SalesGraph.vue";
+
+export default {
+  name: "DashboardPage",
+  components: {
+    SalesGraph,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
